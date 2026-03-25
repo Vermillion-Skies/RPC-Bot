53bc4a9cbbf9b6c6ab87ac671d814060b8780110
Vermillion-Skies <mario4672yt@gmail.com>
2026-03-25 10:15:22 -0400

    Added initial libraries to subscript loader

---
+++ subscripts/subscriptloader.py
@@ -0,0 +1,11 @@
+try:
+    import tkinter as tk
+    from tkinter import ttk
+except Exception as e:
+    print("AN EXCEPTION HAS OCCURRED: " + str(e))
+    print("Hint: You might not have the tkinter library installed!")
+    print("You need that to run this program!")
+    quit()
+import os
+import sys
+import subprocess
