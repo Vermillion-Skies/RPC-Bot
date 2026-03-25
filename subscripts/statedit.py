c2c99859c82058e3266eb058f2e0edb4206f6e62
Vermillion-Skies <mario4672yt@gmail.com>
2026-03-25 11:35:14 -0400

    Initial creation of state editing tool

---
+++ subscripts/statedit.py
@@ -0,0 +1,13 @@
+try:
+    import tkinter as tk
+    from tkinter import filedialog
+    from tkinter import ttk
+except Exception as e:
+    print("AN EXCEPTION HAS OCCURRED: " + str(e))
+    print("Hint: You might not have the tkinter library installed!")
+    print("You need that to run this program!")
+    print("...wait, this is a subprogram of something that also requires tkinter")
+    print("How... How the hell did you manage this??")
+    quit()
+import os
+import sys
