e7740e2c47b07a6899cb1ce836469d709c82d4f4
Vermillion-Skies <mario4672yt@gmail.com>
2026-03-25 11:38:28 -0400

    Implemented savefile function from statmake

--- subscripts/statedit.py
+++ subscripts/statedit.py
@@ -11,3 +11,32 @@ except Exception as e:
     quit()
 import os
 import sys
+def savefile():
+    global fdwindow
+    dir = filedialog.askdirectory()
+    try:
+        with open(str(dir) + "/" + str(name) + ".txt", "w") as f:
+            pass
+        with open(str(dir) + "/" + str(name) + ".txt", "w") as file:
+            file.write("\n".join(entrylist))
+            pass
+        fdwindow = tk.Toplevel(root)
+        fdwindow.title("File save success!")
+        fdwindow.geometry("300x200")
+        fdlabel = tk.Label(fdwindow, text="File has been successfully saved!")
+        fdlabel.pack(padx=5, pady=5)
+        fdbutton = tk.Button(
+            fdwindow,
+            text="Okay!",
+            command=closefdw,
+        )
+        fdbutton.pack(padx=5, pady=5)
+        pass
+    except Exception as e:
+        print("Exception " + str(e) + " has occurred.")
+        quit()
+    pass
+def openfile():
+    pass #Function to open file dialog and ask for a file path
+entrylist = ["null", "null", "null", "null", "null", "null"]
+root = tk.Tk()
