e83255a1d2e098a9b028d8cb56b3d686d7fa40b6
Vermillion-Skies <brtodd6825@eagle.fgcu.edu>
2026-04-01 16:17:41 -0400

    Updated updatewin.bat to use correct file structure

--- subscripts/updatewin.bat
+++ subscripts/updatewin.bat
@@ -1,12 +1,12 @@
-del assets/subscripts/
-del assets/presencebot.py
+del %cd%\assets\subscripts\
+del %cd%\assets\presencebot.py
 echo "Successfully removed old files"
-cd assets/
-mkdir subscripts/
+cd %cd%\assets\
+md %cd%\subscripts\
 echo "Created new subscripts folder"
 echo "Installing lastest stable files..."
 curl "https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/presencebot.py" -o "presencebot.py"
-cd subscripts/
+cd subscripts\
 curl "https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/subscripts/statedit.py" -o "statedit.py"
 curl "https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/subscripts/statmake.py" -o "statmake.py"
 curl "https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/subscripts/subscriptloader.py" -o "subscriptloader.py"
