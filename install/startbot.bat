0489a9512bf34c9f7c4d33ebbe2c9c2cd88ae93a
Vermillion-Skies <brtodd6825@eagle.fgcu.edu>
2026-04-01 18:56:27 -0400

    Fixed issue with startbot.bat where it wouldn't start the bot

--- install/startbot.bat
+++ install/startbot.bat
@@ -1,3 +1,3 @@
-call %cd\env\Scripts\activate.bat
-py %cd\assets\presencebot.py
-call %cd\env\Scripts\deactivate.bat
+CALL %cd%\env\Scripts\activate.bat
+py %cd%\assets\presencebot.py
+CALL %cd%\env\Scripts\deactivate.bat
