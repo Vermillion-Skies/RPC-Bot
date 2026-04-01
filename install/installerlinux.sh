d8583feab30c5ff774c0315ad17ce8fa82a336a9
Vermillion-Skies <brtodd6825@eagle.fgcu.edu>
2026-04-01 15:54:51 -0400

    Updated installer calls to use new install folder

---
+++ install/installerlinux.sh
@@ -0,0 +1,40 @@
+clear
+echo ""
+echo "Vermillion-Skies RPC-Bot installer"
+echo "Starting script..."
+mkdir RPCBot/
+echo "Created RPCBot directory"
+cd RPCBot/
+mkdir assets/
+echo "Created assets directory"
+mkdir assets/statuses/
+echo "Created statuses directory"
+mkdir assets/subscripts/
+echo "Created subscripts directory"
+echo "Creating python environment..."
+python3 -m venv env
+echo "Environment created"
+source env/bin/activate
+echo "Activated environment"
+pip install pypresence tk requests
+echo "Installed dependencies"
+echo "downloading latest stable files..."
+cd assets/
+wget --content-disposition https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/presencebot.py
+cd subscripts/
+wget --content-disposition https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/subscripts/statedit.py
+wget --content-disposition https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/subscripts/statmake.py
+wget --content-disposition https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/subscripts/subscriptloader.py
+cd ..
+cd statuses/
+wget --content-disposition https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/statuses/statustemp.txt
+cd ..
+cd ..
+wget --content-disposition https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/install/updater.sh
+chmod +x updater.sh
+wget --content-disposition https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/install/startbot.sh
+chmod +x startbot.sh
+cd assets/
+echo "Finished installing files"
+echo "Make sure to follow the instructions on GitHub to setup the app properly"
+echo "Ending the installer script"
---
+++ install/installwin.bat
@@ -0,0 +1,38 @@
+clear
+echo ""
+echo "Vermillion-Skies RPC-Bot installer"
+echo "Starting script..."
+mkdir RPCBot/
+echo "Created RPCBot directory"
+cd RPCBot/
+mkdir assets/
+echo "Created assets directory"
+mkdir assets/statuses/
+echo "Created statuses directory"
+mkdir assets/subscripts/
+echo "Created subscripts directory"
+echo "Creating python environment..."
+python -m venv env
+echo "Environment created"
+env/bin/activate
+echo "Activated environment"
+pip install pypresence tk requests
+echo "Installed dependencies"
+echo "downloading latest stable files..."
+cd assets/
+curl "https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/presencebot.py" -o "presencebot.py"
+cd subscripts/
+curl "https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/subscripts/statedit.py" -o "statedit.py"
+curl "https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/subscripts/statmake.py" -o "statmake.py"
+curl "https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/subscripts/subscriptloader.py" -o "subscriptloader.py"
+cd ..
+cd statuses/
+curl "https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/statuses/statustemp.txt" -o "statustemp.txt"
+cd ..
+cd ..
+curl "https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/install/updater.bat" -o "updater.bat"
+curl "https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/install/startbot.bat" -o "startbot.bat"
+cd assets/
+echo "Finished installing files"
+echo "Make sure to follow the instructions on GitHub to setup the app properly"
+echo "Ending the installer script"
