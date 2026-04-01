50bc8c8f5a3ed3ef49e1bdd1d020da8a19ce28e9
Vermillion-Skies <brtodd6825@eagle.fgcu.edu>
2026-04-01 16:16:23 -0400

    Updated updater.bat to use the right directory

--- install/updater.bat
+++ install/updater.bat
@@ -1,3 +1,3 @@
 curl "https://raw.githubusercontent.com/Vermillion-Skies/RPC-Bot/main/subscripts/updatewin.bat" -o "updatewin.bat"
-CALL updatewin.bat
-del updatewin.bat
+CALL %cd%\updatewin.bat
+del %cd%\updatewin.bat
