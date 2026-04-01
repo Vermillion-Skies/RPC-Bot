81952abefaf1dc983ca9eb55363abed2fddb7359
Vermillion-Skies <mario4672yt@gmail.com>
2026-04-01 12:06:56 -0400

    Added all mkdir calls needed in the installer script

--- installer.sh
+++ installer.sh
@@ -1,3 +1,8 @@
+mkdir RPCBot/
+cd RPCBot/
+mkdir statuses/
+mkdr subscripts/
 python3 -m venv env
 source env/bin/activate
-pip install -r 
+pip install pypresence tk
+echo "downloading latest stable files..."
