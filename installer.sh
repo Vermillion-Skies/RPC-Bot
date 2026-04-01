0546f88cb3aaf2c9cc38a46df941b0c257c2a624
Vermillion-Skies <mario4672yt@gmail.com>
2026-04-01 12:18:53 -0400

    Finished main logic of installer, need to change wget parameters

--- installer.sh
+++ installer.sh
@@ -1,8 +1,21 @@
 mkdir RPCBot/
 cd RPCBot/
-mkdir statuses/
-mkdr subscripts/
+mkdir assets/
+mkdir assets/statuses/
+mkdir assets/subscripts/
 python3 -m venv env
 source env/bin/activate
 pip install pypresence tk
 echo "downloading latest stable files..."
+cd assets/
+wget https://github.com/Vermillion-Skies/RPC-Bot/blob/main/presencebot.py
+cd subscripts/
+wget https://github.com/Vermillion-Skies/RPC-Bot/blob/main/subscripts/statedit.py
+wget https://github.com/Vermillion-Skies/RPC-Bot/blob/main/subscripts/statmake.py
+wget https://github.com/Vermillion-Skies/RPC-Bot/blob/main/subscripts/subscriptloader.py
+cd ..
+cd statuses/
+wget https://github.com/Vermillion-Skies/RPC-Bot/blob/main/statuses/statustemp.txt
+cd ..
+echo "Finished installing files, loading program..."
+python3 presencebot.py
