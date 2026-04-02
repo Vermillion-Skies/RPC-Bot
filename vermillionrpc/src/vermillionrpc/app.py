c4a7244c0ec9f18bba363f5e0a37b5984f2374f3
Vermillion-Skies <mario4672yt@gmail.com>
2026-04-02 14:10:45 -0400

    RW-add: Initial skeleton of the app created

--- vermillionrpc/src/vermillionrpc/app.py
+++ vermillionrpc/src/vermillionrpc/app.py
@@ -8,17 +8,33 @@ from toga.style.pack import COLUMN, ROW
 
 class VermillionRPC(toga.App):
     def startup(self):
-        """Construct and show the Toga application.
+        main_box = toga.Box(direction=COLUMN)
+        
+        details_label = toga.Label(
+            "Details: ",
+            margin=(0, 5),
+        )
+        self.details_input = toga.TextInput(flex=1)
+        
+        details_box = toga.Box(direction=ROW, margin=5)
+        details_box.add(details_label)
+        details_box.add(self.details_input)
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+        broadcastbutton = toga.Button(
+            "Start Broadcast",
+            on_press=self.start_broadcast,
+            margin=5
+        )
+
+        main_box.add(details_box)
+        main_box.add(broadcastbutton)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
+    
+    def start_broadcast(self, widget):
+        print(f"Details= {self.details_input.value}")
 
 
 def main():
