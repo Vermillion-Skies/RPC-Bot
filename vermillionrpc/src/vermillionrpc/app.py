8e1d09376b4aaad03e88f4f0b17d0a2adcf1c34b
Vermillion-Skies <mario4672yt@gmail.com>
2026-04-03 11:23:06 -0400

    RW-upd: Changed layout to three boxes

--- vermillionrpc/src/vermillionrpc/app.py
+++ vermillionrpc/src/vermillionrpc/app.py
@@ -4,18 +4,58 @@ Cross-platform Discord presence app
 
 import toga
 from toga.style.pack import COLUMN, ROW, Pack
+from toga.constants import Direction
 
 
 class VermillionRPC(toga.App):
     def startup(self):
-        filecol = toga.Box(style=Pack(margin=(0, 20))) # Column to show status files
-        contentcol = toga.Box(style=Pack(margin=(0, 5))) # Column to show status file contents, as well as broadcast status
-        filecol.add(toga.Label("Status Files"))
-        contentcol.add(toga.Label("Contents"))
-        split = toga.SplitContainer()
-        split.content = [filecol, contentcol]
+        # Column to show status files
+        filecol = toga.Box(
+            style=Pack(
+                margin=(0, 75)
+            )
+        )
+        # Column to show status file contents, as well as broadcast status
+        contentbox = toga.Box(
+            style=Pack(
+                margin=(0, 5)
+            )
+        )
+        # Controls for the RPC broadcast
+        broadcastcont = toga.Box(
+            style=Pack(
+                margin=(0,5),
+            )
+        )
+        # Starts RPC broadcast
+        bcs = toga.Button(
+            "Start broadcast"
+        )
+        # Ends RPC broadcast
+        bce = toga.Button(
+            "End broadcast"
+        )
+        broadcastcont.add(
+            bcs,
+            bce
+        )
+        filecol.add(toga.Label("Statuses"))
+        contentbox.add(toga.Label("Contents"))
+        rightsplit = toga.SplitContainer(
+            content=[
+                contentbox,
+                broadcastcont
+            ],
+            direction=Direction.HORIZONTAL
+        )
+        fullsplit = toga.SplitContainer(
+            content=[
+                filecol,
+                rightsplit
+            ]
+        )
         main_box = toga.Box()
-        main_box.add(split)
+        main_box.add(fullsplit)
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
