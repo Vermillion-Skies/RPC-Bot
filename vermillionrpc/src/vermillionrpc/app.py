1019ff35c9737440f2d79b57eb8b124743c252f4
Vermillion-Skies <mario4672yt@gmail.com>
2026-04-03 10:48:38 -0400

    RW-add: Created new initial layout

--- vermillionrpc/src/vermillionrpc/app.py
+++ vermillionrpc/src/vermillionrpc/app.py
@@ -3,19 +3,19 @@ Cross-platform Discord presence app
 """
 
 import toga
-from toga.style.pack import COLUMN, ROW
+from toga.style.pack import COLUMN, ROW, Pack
 
 
 class VermillionRPC(toga.App):
     def startup(self):
-        """Construct and show the Toga application.
-
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
+        filecol = toga.Box(style=Pack(margin=(0, 20))) # Column to show status files
+        contentcol = toga.Box(style=Pack(margin=(0, 5))) # Column to show status file contents, as well as broadcast status
+        filecol.add(toga.Label("Status Files"))
+        contentcol.add(toga.Label("Contents"))
+        split = toga.SplitContainer()
+        split.content = [filecol, contentcol]
         main_box = toga.Box()
-
+        main_box.add(split)
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
